37f68bfe7ce443c3a28cd34ee3abadb78bd44c8f
dteslab <talesfranca.97@gmail.com>
2023-06-05 10:57:14 -0300

    Inicializando com tela de selecao de funcionalidade

---
+++ myMarket.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	int endCode, opcaoFunction;
+	
+	do{
+		
+		printf("_____________");
+		printf("myMarkey");
+		printf("______________");
+		printf("\n1- VISUALIZAR ESTOQUE:");
+		printf("\n2- ADICIONAR/REMOVER PRODUTO:");
+		printf("\n3- VENDAS:");
+		printf("\n0- PARA FINALIZAR O PROGRAMA.");
+		
+		printf("\nSelecione a opcao desejada: ");
+		scanf("%d", &opcaoFunction);
+		
+		switch(opcaoFunction){
+			case 1:
+				printf("\nVOCE SELECIONOU VISUALIZAR ESTOQUE;\n");
+				break;
+			case 2:
+				printf("\nVOCE SELECIONOU ADICIONAR/REMOVER PRODUTO;\n");
+				break;
+			case 3:
+				printf("\nVOCE SELECIONOU VENDAS;\n");
+		}
+		
+	}while(opcaoFunction != 0);
+}
