eb6555a89b0be407b6c1065d93fed3844769c47f
dteslab <talesfranca.97@gmail.com>
2023-06-06 12:55:10 -0300

    inicializando funcionalidade de cadastro de produtos

--- myMarket.cpp
+++ myMarket.cpp
@@ -5,32 +5,63 @@
 int main(){
 	int opcaoFunction;
 	
+	char nomeProd[50];
+	int i, codigoProd, quantProd;
+	float precoProd;
 	
+	int maisProd;
+	char voltarMenu[30];
 	
 	do{
 		
-		printf("_____________");
-		printf("myMarket");
-		printf("______________");
-		printf("\n1- VISUALIZAR ESTOQUE:");
-		printf("\n2- ADICIONAR/REMOVER PRODUTO:");
-		printf("\n3- VENDAS:");
-		printf("\n0- PARA FINALIZAR O PROGRAMA.");
+		printf("\n_____________\n");
+		printf("\nmyMarket\n");
+		printf("\n______________\n");
+		printf("\n1- VISUALIZAR ESTOQUE: \n");
+		printf("\n2- ADICIONAR/REMOVER PRODUTO: \n");
+		printf("\n3- VENDAS: \n");
+		printf("\n0- PARA FINALIZAR O PROGRAMA. \n");
 		
-		printf("\nSelecione a opcao desejada: ");
+		printf("\nSELECIONE A OPCAO DESEJADA: \n");
 		scanf("%d", &opcaoFunction);
 		
 		system("cls");
 		
 		switch(opcaoFunction){
 			case 1:
-				printf("\nVOCE SELECIONOU VISUALIZAR ESTOQUE;\n");
+				do{
+				printf("\nVOCE SELECIONOU VISUALIZAR ESTOQUE;\n");				
+				printf("\nPRODUTO: %s\n", nomeProd);
+				printf("\nCODIGO DO PRODUTO: %d\n", codigoProd);
+				printf("\nPRECO: %.2f\n", precoProd);
+				printf("\nQUANTIDADE: %d\n", quantProd);
+				printf("\nDIGITE SAIR PARA VOLTAR AO MENU PRINCIPAL: \n");
+				scanf("%s", &voltarMenu);
+				}while(voltarMenu == "Sair" || voltarMenu == "SAIR");
 				break;
+				
 			case 2:
 				printf("\nVOCE SELECIONOU ADICIONAR/REMOVER PRODUTO;\n");
+				
+				do{
+				printf("\nDIGITE NOME DO PRODUTO: ");
+				scanf("%s", &nomeProd);
+				printf("\nCODIGO DO PRODUTO: ");
+				scanf("%d", &codigoProd);
+				printf("\nPRECO DO PRODUTO: ");
+				scanf("%f", &precoProd);
+				printf("\nQUANTIDADE DO PRODUTO: ");
+				scanf("%d", &quantProd);
+				
+				printf("\nDESEJA CADASTRAR MAIS PRODUTOS? \n");
+				printf("\n1- SIM \n 2- NAO ");
+				scanf("%d", &maisProd);
+				}while(maisProd == 1);			
 				break;
+				
 			case 3:
 				printf("\nVOCE SELECIONOU VENDAS;\n");
+				break;
 		}
 	}while(opcaoFunction != 0);
 }
