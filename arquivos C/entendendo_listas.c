b52daa50768a08649ca26ac69abf9e24c9a73ee7
dteslab <talesfranca.97@gmail.com>
2023-06-16 17:14:08 -0300

    praticando listas

--- arquivos C/entendendo_listas.c
+++ arquivos C/entendendo_listas.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct node {
-    int data;
-    struct node *next;
-} node;
+typedef struct meuNo {
+    int info;
+    struct meuNo* prox;
+} meuNo;
 
-int main(){
-    //Estruturando a cabeca do no;
-    struct node *head = malloc(sizeof(struct node));
-    head->data = 0;
-    head->next = NULL;
-    
-    struct node *new_node = malloc(sizeof(struct node));
-    new_node->data = 1;
-    new_node->next = NULL;
-    head->next = new_node;
-    
-    struct node *current = head;
-    while (current != NULL){
-        printf("%d\n", current->data);
-        current = current->next;
+/* Alocando uma função de criação do nó e apontando para a sua estrutura;
+ * observa-se que a variável (novoNo) está sendo apontada e alocada em um espaço de memória da estrutura do nó;
+ * a variável (novoNo) está apontando para a váriavel (info) onde armazenará o valor do nó;
+ * a mesma variável (novoNo) está apontando para o ponteiro que estará direcionando para a próxima estrutura de um nó;*/
+meuNo* criarNo(int info) {
+    meuNo* novoNo = (meuNo*) malloc(sizeof(meuNo)); // aloca um espaço de memória no formato da estrutura criada do nó;
+    novoNo->info = info; // "chama" a variável alocada e aponta o seu valor;
+    novoNo->prox = NULL; // "chama" a variavel alocada e define seu ponteiro, onde este estará apontando para um próximo elemento nó;
+    return novoNo; // retorna o nó criado;
+}
+
+void inserirNaLista (meuNo** head, int info) {
+    meuNo* novoNo = criarNo(info);
+    novoNo->prox = NULL;
+    if (*head == NULL) {
+        *head = novoNo;
+    } else { 
+        meuNo* atual = *head;
+        while (atual->info != NULL) { 
+            atual = atual->prox;
+        }
+        atual->prox = novoNo;
     }
 }
--- arquivos C/mercado_estoque/elementosDoEstoque.c
+++ arquivos C/mercado_estoque/elementosDoEstoque.c
@@ -2,14 +2,83 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
+typedef struct Produtos {
     int idProduto;
-    int quantProdutod;
-    char nomeProduto[30];
+    int quantidadeProduto;
+    char nomeProduto[50];
+    char tipoProduto[50];
     float precoProduto;
 } Produtos;
 
-typedef struct nohProduto {
+typedef struct noProduto {
     int value;
-    struct nohProdut *next;
-} nohProduto;
+    struct noProduto* next;
+} noProduto;
+
+noProduto* criarNo(int value){
+    noProduto* novoNo = (noProduto*) malloc(sizeof(noProduto));
+    novoNo->value = value;
+    novoNo->next = NULL;
+    return novoNo;
+}
+
+void inserirInicio(noProduto** head, int value) {
+    noProduto* novoNo = criarNo(value);
+    novoNo->next = *head;
+    *head = novoNo;
+}
+
+void inserirFinal(noProduto** head, int value){
+    noProduto* novoNo = (noProduto*) malloc(sizeof(noProduto));
+    novoNo->value = value;
+    novoNo->next = NULL;
+
+    if(*head == NULL){
+        *head = novoNo;
+    } else {
+        noProduto* current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = novoNo;
+    }
+}
+
+void imprimirLista(noProduto* head) {
+    noProduto* current = head;
+    while(current != NULL) {
+        printf("%d\n", current->value);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+void removerInicio(noProduto** head) {
+    if(*head == NULL) {
+        printf("Lista vazia.");
+        return;
+    }
+    noProduto* current = *head;
+    *head = (*head)->next;
+    free(current);
+}
+
+int main() {
+    noProduto* lista = NULL;
+
+/*  inserirInicio(&lista, 10);
+    inserirInicio(&lista, 5);
+    inserirInicio(&lista, 0); */
+
+    inserirFinal(&lista, 2);
+    inserirFinal(&lista, 4);
+    inserirFinal(&lista, 6);
+    inserirFinal(&lista, 8);
+    inserirFinal(&lista, 10);
+    imprimirLista(lista);
+
+/*  removerInicio(&lista);
+    imprimirLista(lista); */
+
+    return 0;
+}
